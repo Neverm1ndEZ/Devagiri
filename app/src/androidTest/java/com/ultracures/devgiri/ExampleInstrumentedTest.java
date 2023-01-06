42932ce3f92c4950de1ec0a51d81e0f71f8117d2
mihirsuman0131@gmail.com <mihirsuman0131@gmail.com>
2023-01-06 13:49:24 +0530

    enhancements

--- app/src/androidTest/java/com/ultracures/devgiri/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ultracures/devgiri/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.devgiri;
+package com.ultracures.devgiri;
 
 import android.content.Context;
 
--- app/src/test/java/com/ultracures/devgiri/ExampleUnitTest.java
+++ app/src/test/java/com/ultracures/devgiri/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.devgiri;
+package com.ultracures.devgiri;
 
 import org.junit.Test;
 
